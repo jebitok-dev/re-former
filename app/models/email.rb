904c0fda291dc15fe4004b03fbe8b43885e3d7f4
Jebitok <sharonkosgei4@gmail.com>
2021-02-16 12:08:54 +0300

    add validation models

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,6 @@
 class Email < ApplicationRecord
+    belongs_to :user
+    belongs_to :password
+
+    validates :email, presence: true, length: { maximum: 30 }
 end
--- app/models/password.rb
+++ app/models/password.rb
@@ -1,2 +1,6 @@
 class Password < ApplicationRecord
+    belongs_to :user
+    belongs_to :email
+
+    validates :password, presence: true, length: {minimum: 8}
 end
