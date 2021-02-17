77a13020427ffb24e06dde7875f615fabb76974f
Jebitok <sharonkosgei4@gmail.com>
2021-02-17 13:56:56 +0300

    fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(params_user)
 
@@ -25,7 +27,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-   
+
   private
 
   def params_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, confirmation: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
 end
